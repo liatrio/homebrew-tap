73ea592f4f8cae3423f3fc303403ee1e04d1664f
goreleaserbot <goreleaser@carlosbecker.com>
2021-01-05 12:33:55 -0800

    Brew formula update for gitty-up version v0.1.5

--- gitty-up.rb
+++ gitty-up.rb
@@ -2,16 +2,16 @@
 class GittyUp < Formula
   desc ""
   homepage ""
-  version "0.1.4"
+  version "0.1.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/liatrio/gitty-up/releases/download/v0.1.4/gitty-up_0.1.4_Darwin_x86_64.tar.gz"
-    sha256 "9eb4b7f039cbc7208b18c113161307df0708039d78ce5bf425aea7834cad1e0d"
+    url "https://github.com/liatrio/gitty-up/releases/download/v0.1.5/gitty-up_0.1.5_Darwin_x86_64.tar.gz"
+    sha256 "327fe8ccac303767ebf7791a1fadd33186c8675ac508aeb07dc80d13a0582807"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/liatrio/gitty-up/releases/download/v0.1.4/gitty-up_0.1.4_Linux_x86_64.tar.gz"
-      sha256 "28c81b6c27f6a09cc11c274a6818e34d9d4815377a4fcf053f5c857a9c693423"
+      url "https://github.com/liatrio/gitty-up/releases/download/v0.1.5/gitty-up_0.1.5_Linux_x86_64.tar.gz"
+      sha256 "0adb82e4aea3deaaf232ad929c81a99036c22493ecc4612c09decafef7206c50"
     end
   end
 
